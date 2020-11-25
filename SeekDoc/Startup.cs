81355b59dfc85636c55b40c8680d43d036214587
dbcxx <40644301+dbcxx@users.noreply.github.com>
2020-11-25 17:57:26 +0100

    first commit to login and registration

---
+++ SeekDoc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SeekDoc.Startup))]
+namespace SeekDoc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
